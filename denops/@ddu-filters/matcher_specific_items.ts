84bd4bc2411285988a7964b228e49ea04b5d21e5
staticWagomU <to6wa17@gmail.com>
2024-08-02 13:09:40 +0900

    refactor: jsr移行

--- denops/@ddu-filters/matcher_specific_items.ts
+++ denops/@ddu-filters/matcher_specific_items.ts
@@ -2,8 +2,8 @@ import {
   BaseFilter,
   DduItem,
   SourceOptions,
-} from "https://deno.land/x/ddu_vim@v4.0.0/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v4.0.0/deps.ts";
+} from "jsr:@shougo/ddu-vim@^5.0.0/types";
+import { Denops } from "jsr:@denops/core@^7.0.0";
 
 type Params = {
   startsWith: string;
